d361877d7e3b31d69359225cff278b3cd84fb834
Celeste Cruz <celesteglezcruz@gmail.com>
2025-02-17 21:16:03 -0500

    inicializando repositorio frontend

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+function AppRoutes() {
+
+  return (
+    <>
+      <h1 className='text-2xl font-bold text-slate-800'>Hola, ya está completada la instalación de tailwindCSS</h1>
+    </>
+  )
+}
+
+export default AppRoutes;
